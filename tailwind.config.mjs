9de2df6026b4463b17cc7a67ec6cc8e00886c96c
Adrián Benítez Roldán <149155965+AdrianBR003@users.noreply.github.com>
2024-12-29 20:59:21 +0100

    Init Commit - create Comp Header and Badge

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class', // Modo oscuro por defecto
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
